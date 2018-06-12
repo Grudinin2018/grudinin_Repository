4240aec443ef38d40fa23ae66453a56b86e9c3d2
dimon <lol@lol.ru>
2018-06-12 23:48:44 +0500

    govnokod

--- index.php
+++ index.php
@@ -111,36 +111,185 @@
           if (value.need != '' && value.date != '') {
             var name = key.split('_')[0];
             var type = key.split('_')[1];
+
+            var list = getList(name);
+            list.need = Number(list.need);
+            list.stock = Number(list.stock);
+
+            var detki = getDetki(name);
+            detki.need = Number(detki.need);
+            detki.stock = Number(detki.stock);
+
+            var cvet = getCvet(name);
+            cvet.need = Number(cvet.need);
+            cvet.stock = Number(cvet.stock);
+
+            var nowtime = new Date ();
+            var date = new Date (value.date);
+
+            function fix(arg) { if (String(arg).length == 1) return '0'+arg; else return String(arg); }
+
             if (type == 'цветущая')
             {
-              if (value.need > value.stock) {
-                console.log(key + ' Idi nahuy');
+              if (value.need > value.stock) { //начинаем проверять детки и листы
+                  console.log(key);
+                  console.log('Цветущих требуется: ' + cvet.need);
+                  console.log('Цветущих на складе: ' + cvet.stock);
+                  console.log('Деток на складе: ' + detki.stock);
+                  console.log('Листков на складе: ' + list.stock);
 
-                //Количество list
-                var list = getList(name);
-                console.log(list.stock); 
-                console.log(list.need);
+                  var diff = value.need - value.stock; // здесь сколько не хватает
+                  var divTime = (date.getTime()-nowtime.getTime())/1000/60/60/24;
+
+                  //высадить до
+                  var ddt = date.getTime()-(180*1000*60*60*24);
+                  var ddtDate2 = new Date(ddt);
+                  console.log(ddtDate2.toISOString());
+
+                  if ((divTime>=90)) { //если деткам хватит времени вырасти
+                    diff = diff - detki.stock;
+                    console.log(diff); 
+                    if (diff <= 0) {  //если деток хватило, то садить не надо
+                      console.log('Успех!');
+                      console.log('Листья садить не нужно, ' + (cvet.need-cvet.stock) + 'шт. деток со склада успеют вырасти. Зарезервируйте их.');
+                    }
+                    else {
+                      if (divTime>=180) {  // проверяем есть и хватит ли листков и времени вырасти
+                        diff = diff - list.stock;
+                          if (diff <=0) {         //листков на складе хватило
+                            console.log('Успех!');
+                            console.log('Листков на складе хватило, вам надо отложить ' + (cvet.need-cvet.stock-detki.stock) + 'шт. и посадить до' + ddtDate2); //здесь нужно получить крайнюю дату посадки листков
+                          }
+                          else { //листков на складе не хватило
+                            console.log('Не получится! Листков на складе не хватило!')
+                            console.log('Нужно где-то найти и посадить' + (cvet.need-cvet.stock-detki.stock-list.stock) + ' листков до ' + ddtDate2); //здесь вконце надо вставить дату край, когда успеют еще вырасти
+                          }
+                      }
+                      else { //если листков нет (не хватает) на складе, либо они не вырастут по времени
+                        console.log('Не получится!');
+                        console.log('Цветущих и деток не хватает, а новые не успеют вырасти');
+                      }
+                    }
+                  }
+                  else {
+                    console.log('Не получится!');
+                    console.log('Цветущих фиалок не хватает, а новые не успеют вырасти');
+                  }
+              }
+              else { 
+                console.log(key);
+                console.log('Успех!');
+                console.log('Вам хватает цветущих фиалок на складе');
+                console.log('Цветущих требуется: '+(cvet.need));
+                console.log('Цветущих на складе: '+(cvet.stock));
               }
             } 
             else if (type == 'детки')
             {
-              if (value.need > value.stock) {
-                console.log(key + ' Idi nahuy');
+              if (value.need > value.stock) { //начинаем проверять детки и листы
+                  console.log(key);
+                  console.log('Деток требуется: ' + detki.need);
+                  console.log('Деток на складе: ' + detki.stock);
+                  console.log('Листков на складе: ' + list.stock);
+
+                  var diff = value.need - value.stock; // здесь сколько не хватает
+                  var divTime = (date.getTime()-nowtime.getTime())/1000/60/60/24;
+
+                  //высадить до
+                  var ddt = date.getTime()-(90*1000*60*60*24);
+                  var ddtDate2 = new Date(ddt);
+                  console.log(ddtDate2.toISOString());
+                  console.log(fix(ddtDate2.getDate())+'.'+(fix(ddtDate2.getMonth()+1))+'.'+ddtDate2.getFullYear());
+                  //console.log(nowtime.getDate()+'.'+nowtime.getMonth()+'.'+nowtime.getFullYear());
+                  //$.datepicker.parseDate(ddtDate2.toISOString());
+
+                  if ((divTime>=90)) { //если листьям хватит времени вырасти до деток
+                    diff = diff - list.stock;
+                    console.log(diff); 
+                    if (diff <= 0) {  //если листьев хватило, то искать не надо
+                      console.log('Успех!');
+                      console.log('Листьев на складе хватает, ' + (detki.need-detki.stock) + 'шт. листьев со склада успеют вырасти. Зарезервируйте и посадите их до ' + ddtDate2);
+                    }
+                    else {
+                            console.log('Не получится! Листков на складе не хватило!')
+                            console.log('Нужно где-то найти и посадить' + (detki.need-detki.stock-list.stock) + ' листков до ' + ddtDate2); //здесь вконце надо вставить дату край, когда успеют еще вырасти
+                    }
+                  }
+                  else {
+                    console.log('Не получится!');
+                    console.log('Деток на складе не хватает, а новые не успеют вырасти');
+                  }
+              }
+              else { 
+                console.log(key);
+                console.log('Успех!');
+                console.log('Вам хватает деток на складе');
+                console.log('Деток требуется: '+ (detki.need));
+                console.log('Деток на складе: '+ (detki.stock));
               }
             }
+
             if (type == 'лист')
             {
-              if (value.need > value.stock) {
-                console.log(key + ' Idi nahuy');
-                var xyi = 0;
-                var nowtime = new Date ();
-                var date = new Date (value.date);
-                console.log(date.getTime()-nowtime.getTime());
-                var divTime = (date.getTime()-nowtime.getTime())/1000/60/60/24;
-                console.log(divTime);
+              if (value.need > value.stock) { //начинаем проверять детки и листы
+                  console.log(key);
+                  console.log('Листков требуется: ' + list.need);
+                  console.log('Листков на складе: ' + list.stock);
+                  console.log('Цветущих на складе: ' + cvet.stock);
+                  console.log('Деток на складе: ' + detki.stock);
 
+                  var diff = value.need - value.stock; // здесь сколько не хватает
+                  var divTime = (date.getTime()-nowtime.getTime())/1000/60/60/24;
 
-                
+                  //высадить до
+                  var ddt = date.getTime()-(90*1000*60*60*24);
+                  var ddtDate2 = new Date(ddt);
+                  //console.log(ddtDate2.toISOString());
+                  //console.log(ddtDate2.getDate()+'.'+ddtDate2.getMonth()+'.'+ddtDate2.getFullYear());
+                  //$.datepicker.parseDate(ddtDate2);
+
+                  if ((diff-(cvet.stock*20))<=0) { // листьев и цветущих хватит.
+                    if (list.stock>0) {
+                      console.log('Успех!');
+                      console.log('Сейчас есть '+list.stock+' листков'); 
+                      console.log('Отломите еще '+diff+' листков от '+ (cvet.stock-Math.floor((cvet.stock*20-diff)/20))+' цветущих'); 
+                    }
+                    else {
+                      console.log('Успех!');
+                      console.log('Просто отломите '+diff+' листков от '+ (cvet.stock-Math.floor((cvet.stock*20-diff)/20))+' цветущих'); 
+                    }
+                  }
+                  else { // если листьев и цветущих не хватило проверяем
+                    if (divTime>=90) { //хватает ли времени
+                      if ((diff-cvet.stock*20-detki.stock*20)<=0) { //если кол-ва на складе хватает, а не успеваем только по дате
+                        if (list.stock>0) {
+                          console.log('Сейчас есть '+list.stock+' листков');
+                          if (cvet.stock>0) {
+                            console.log('Сейчас есть '+cvet.stock+' цветущих фиалок, из них получится '+cvet.stock*20+' листков');
+                          }
+                        }
+                        console.log('Отломите еще '+diff+' листков от '+ (cvet.stock-Math.floor((cvet.stock*20-diff)/20))+' цветущих'); 
+                      }
+                    }
+                    else {
+                      if ((detki.stock>0) && ((diff-cvet.stock*20-detki.stock*20)<=0)){
+                        console.log('Не получится!');
+                        console.log('Листков и цветов на складе не хватает, чтобы получить листья к указанной дате, а '+(Math.ceil((diff-cvet.stock*20)/20))+' шт. деток не успеют вырасти. Предлагаем сдвинуть дату на ... дней');
+                        console.log('Из них нужно оторвать '+(diff-cvet.stock*20)+' листков');
+                      }
+                      else {
+                        console.log('Не получится!');
+                        console.log('Листков, цветов и деток на складе не хватает');
+                      }
+                      }
+                  }
+              }
+              else { 
+                console.log(key);
+                console.log('Успех!');
+                console.log('Вам хватает листков на складе');
+                console.log('Листков требуется: '+ (list.need));
+                console.log('Листков на складе: '+ (list.stock));
               }
             }
           }
